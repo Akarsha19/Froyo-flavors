fb52ada0240b5fdcfdecc964d2ccc445885a2b3c
Akarsha19 <akarshap19@gmail.com>
2024-10-16 12:57:39 -0400

    finished workshop

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 const userInput = prompt('Please enter your desired flavors using a comma between each one.')
 //Make sure to split by commas 
 const userInputArray = userInput.split(`,`);
-console.log(userInputArray);
 
 //create empty object
 const flavors = {};
 
+//use loop to add flavors 
 for (let i = 0; i < userInputArray.length; i++) {
-
+  const desiredFlavor = userInputArray[i];
+  if (!flavors[desiredFlavor]) {
+    flavors[desiredFlavor] = 1;
+  }
+  else {
+    flavors[desiredFlavor] += 1;
+  }
+  console.log(flavors);
 }
